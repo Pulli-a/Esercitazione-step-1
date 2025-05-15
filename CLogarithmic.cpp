a4dd0cef1c0fac770be44d70f35d44bdcb460c17
pulli <pu.maryni@gmail.com>
2025-05-15 18:10:21 +0200

    stabil version --testd

--- CLogarithmic.cpp
+++ CLogarithmic.cpp
@@ -18,20 +18,24 @@ using namespace std;
 Logarithmic::Logarithmic(){
 	impostacoef(10, 0.0);
 }
+
 ///@brief copia-costruttore
 Logarithmic::Logarithmic(const Logarithmic &l){
 	impostacoef(l.b_coeff, l.k_coeff);
 }
+
 ///@brief costruttore non di default
 ///@param b parametro base
 ///@param k parametro coefficente
 Logarithmic::Logarithmic(double b, double k){
 	impostacoef(b, k);
 }
+
 ///@brief distruttore di default
 Logarithmic::~Logarithmic(){
 	//non ho puntatori faccio un eliminazione shallow
 }
+
 ///@brief overloading operatore =
 Logarithmic& Logarithmic::operator=(const Logarithmic& l){
 	if(this == &l){
@@ -42,6 +46,7 @@ Logarithmic& Logarithmic::operator=(const Logarithmic& l){
 		return *this;
 	}
 }
+
 ///@brief overloading operatore ==
 bool Logarithmic::operator==(const Logarithmic& l){
 	if(k_coeff == l.k_coeff && b_coeff == l.b_coeff){
@@ -50,21 +55,24 @@ bool Logarithmic::operator==(const Logarithmic& l){
 		return false;
 	}
 }
+
 ///@brief GetValue restituisce il valore di y data x
 ///@param x
 ///@return y
 double Logarithmic::GetValue(double in){
-	if(in > 0){
-		return k_coeff*(log2(in) / log2(b_coeff));
-	}else{
-		ErrorMessage("valore di x in un log puo' essere solo > 0");
+	if(in < 0){
+		ErrorMessage("'in' value should be > 0");
+		return 0;
 	}
+	return k_coeff*(log2(in) / log2(b_coeff));
 }
+
 ///@brief imposta il valore k
 ///@param k
 void Logarithmic::impostak(double k){
 	k_coeff = k;
 }
+
 ///@brief imposta il valore b
 ///@param b
 void Logarithmic::impostab(double b){
@@ -76,6 +84,7 @@ void Logarithmic::impostab(double b){
 		return;
 	}
 }
+
 ///@brief imposta il valore k
 ///@param b
 ///@param k
@@ -85,11 +94,13 @@ void Logarithmic::impostacoef(double b, double k){
 		k_coeff = k;
 		return;
 	}else{
-		cout<<"errore impostando base, deve essere >0"<<endl<<flush;
+		cout<<"errore impostando base, deve essere >0"<< endl <<flush;
 		return;
 	}
 }
+
 ///@brief dump dei dati
 void Logarithmic::Dump(){
-	cout<<"dumping funzione logaritmica"<<endl<<"base: "<< b_coeff<<" coefficente: "<<k_coeff<<endl<<flush;
+	cout<<"Dump of Logarithmic"<<endl;
+	cout << k_coeff << "log" << b_coeff << "(x)"<< endl << flush;
 }
--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
-#include "CFunction.h"
 #include "CPolynomial.h"
 #include "CExponential.h"
 #include "CLogarithmic.h"
+#include "Cpower.h"
 
 
 int main(){
-	Function* array[4];
+	Function* array[7];
+	int len = 7;
 	Exponential E1;
 	Exponential E2(1, 2 , 1);
 	Logarithmic L1(10, 5);
 	Logarithmic L2;
-	
+	Power P0;
+	Power P1(-2, 4);
+	Power P2(1, 0);
+
+	E1.SetExponential(1, 2, 0);
+
 	array[0] = &E1;
 	array[1] = &E2;
 	array[2] = &L1;
-	array[3] = &L2; 
-	
-	for(int i = 0; i < 4; i++){
-		cout<<"valore della funzione in x = 2"<<array[i]->GetValue(2)<<endl<<flush;
+	array[3] = &L2;
+	array[4] = &P0;
+	array[5] = &P1;
+	array[6] = &P2;
+
+	for(int i = 0; i < len; i++){
+		array[i]->Dump();
+		cout << "value in x= 3 : " << array[i]->GetValue(3.) << endl << endl ;
 	}
+
+	cout << "##########################################" << endl;
+
+	E2.SetBCoeff(-1);
+
+	for(int i = 0; i < len; i++){
+		array[i]->Dump();
+		cout << "value in x= -5 : " << array[i]->GetValue(3.) << endl << endl ;
+	}
+
+	return 0;
 
 }
